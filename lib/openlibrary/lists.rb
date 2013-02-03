7168947263ce32f798e25490aa107c75a9cfc2a7
Charles Horn <charles.horn@gmail.com>
2013-02-03 16:31:35 +1100

    add tests for Lists (collection) and List classes

--- lib/openlibrary/lists.rb
+++ lib/openlibrary/lists.rb
@@ -1,20 +1,21 @@
 module Openlibrary
 
   class Lists
-    attr_accessor :entries
+    attr_accessor :links, :size, :entries
 
     def self.find_by_user(user)
       request_url = "http://openlibrary.org/people/#{user}/lists.json"
-      response = RestClient.get request_url
-      response_data = JSON.parse(response)
-      entries = response_data['entries']
-      if entries
-        list_entries = new
-        list_entries.entries = entries.collect {|e| List.new(e)}
-        list_entries
-      else
-        puts "OPENLIBRARY: No lists found for user #{user} on Openlibrary.org"
-        nil
+      lists = new
+      begin
+        response = RestClient.get request_url
+        response_data = JSON.parse(response)
+        lists.entries = response_data['entries'].collect {|e| List.new(e)}
+        lists.size = response_data['size']
+        lists.links = response_data['links']
+        lists
+      rescue Exception => e
+        puts "Unable to get lists for user '#{user}': #{e.http_code}"
+        return nil
       end
     end
 
